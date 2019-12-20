fb404d433a46cc24a8674d1ede3ffa1498cb0e61
Daniel <daniel.morales@mantum.com.co>
2019-12-20 09:23:36 -0500

    First

---
+++ src/main/java/com/mantum/lib/Person.java
@@ -0,0 +1,15 @@
+package com.mantum.lib;
+
+public class Person {
+  private Integer id;
+
+  private String name;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
